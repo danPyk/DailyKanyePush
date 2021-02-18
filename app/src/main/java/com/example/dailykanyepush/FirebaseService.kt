01a8d528509c9db5695078f72aba2fc54f7b8208
danPyk <dan.pykalo@gmail.com>
2021-02-18 11:20:19 +0100

    push work in foreground

---
+++ app/src/main/java/com/example/dailykanyepush/FirebaseService.kt
@@ -0,0 +1,74 @@
+package com.example.dailykanyepush
+
+import android.app.NotificationManager
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.dailykanyepush.receiver.sendNotification
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class FirebaseService: FirebaseMessagingService() {
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    override fun onMessageReceived(p0: RemoteMessage) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${p0?.from}")
+
+        //check messages for data
+        // Check if message contains a data payload.
+        p0?.data?.let {
+            Log.d(TAG, "Message data payload: " + p0.data)
+        }
+
+        // check messages for notification and call sendNotification
+/*        // Check if message contains a notification payload.
+  */      p0?.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            if(true==false){
+                    sendNotification(it.body!!)
+
+                }
+        }
+    }
+
+    //TODO Step 3.2 log registration token
+    // [START on_new_token]
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(p0: String) {
+        Log.d(TAG, "Refreshed token is $p0")
+        sendRegistrationToServer(p0)
+    }
+    // [END on_new_token]
+    /**
+     * Persist token to third-party servers.
+     *
+     * @param token The new token.
+     */
+    private fun sendRegistrationToServer(token: String?) {
+        // TODO: Implement this method to send token to your app server.
+    }
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private fun sendNotification(messageBody: String) {
+        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+        notificationManager.sendNotification(messageBody, applicationContext)
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
+}
--- app/src/main/java/com/example/dailykanyepush/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/dailykanyepush/ui/home/HomeFragment.kt
@@ -1,16 +1,29 @@
 package com.example.dailykanyepush.ui.home
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.dailykanyepush.R
+import com.google.firebase.messaging.FirebaseMessaging
+
+//first page with info
+// report bug, buy bear, contact
+
+//delete one cart
+//first deploy on phone, then thinking how to manage hours
 
 class HomeFragment : Fragment() {
+    private val TOPIC = "Daily-Kanye"
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -22,10 +35,61 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        //change to setonclicklistner
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        createChannel(
+            getString(R.string.egg_notification_channel_id),
+            getString(R.string.egg_notification_channel_name)
+        )
+        createChannel(
+            getString(R.string.breakfast_notification_channel_id),
+            getString(R.string.breakfast_notification_channel_name)
+        )
+        subscribeTopic()
         return root
+    }
+    private fun createChannel(channelId: String, channelName: String) {
+        // TODO: Step 1.6 START create a channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                // TODO: Step 2.4 change importance
+                NotificationManager.IMPORTANCE_HIGH
+            )
+                // TODO: Step 2.6 disable badges for this channel
+                .apply {
+                    setShowBadge(false)
+                }
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(R.string.breakfast_notification_channel_description)
+
+            val notificationManager = requireActivity().getSystemService(
+                NotificationManager::class.java
+            )
+
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
+        // TODO: Step 1.6 END create channel
+    }
+    private fun subscribeTopic() {
+        // [START subscribe_topic]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var message = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    message = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
     }
 }
